baf18eafd17d2f98e587176c53ba78e3e1a0b8ff
Jcailli <jeanbaptiste.caillieret@ynov.com>
2023-12-15 11:39:53 +0100

    creation de l'entité Pokemon

---
+++ src/Entity/Pokemon.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PokemonRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PokemonRepository::class)]
+class Pokemon
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
+    #[ORM\Column]
+    private ?int $point_de_vie = null;
+
+    #[ORM\Column]
+    private ?int $point_attaque = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getPointDeVie(): ?int
+    {
+        return $this->point_de_vie;
+    }
+
+    public function setPointDeVie(int $point_de_vie): static
+    {
+        $this->point_de_vie = $point_de_vie;
+
+        return $this;
+    }
+
+    public function getPointAttaque(): ?int
+    {
+        return $this->point_attaque;
+    }
+
+    public function setPointAttaque(int $point_attaque): static
+    {
+        $this->point_attaque = $point_attaque;
+
+        return $this;
+    }
+}
