884ed3b8682f2a621523f3ddab2a1452ddf99a6f
Jcailli <jeanbaptiste.caillieret@ynov.com>
2023-12-20 14:40:00 +0100

    MAJ TypeController

--- src/Controller/TypeController.php
+++ src/Controller/TypeController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/type')]
+#[Route('pokedex/type')]
 class TypeController extends AbstractController
 {
     #[Route('/', name: 'app_type_index', methods: ['GET'])]
