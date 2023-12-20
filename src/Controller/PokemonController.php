356b4f328e7f4b372002eb84cc58ef690c0a557e
Jcailli <jeanbaptiste.caillieret@ynov.com>
2023-12-20 14:39:48 +0100

    MAJ PokemonController

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/pokemon')]
+#[Route('/pokedex/pokemon')]
 class PokemonController extends AbstractController
 {
     #[Route('/', name: 'app_pokemon_index', methods: ['GET'])]
